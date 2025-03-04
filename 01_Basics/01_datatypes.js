b69c43d8f1e8d555a259d493d0e65b96a40a8e17
Manas Bhardwaj <102716939+Manas-277@users.noreply.github.com>
2025-03-04 18:01:18 +0000

    added 2 files

---
+++ 01_Basics/01_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat it as newer version of JS by the engine
+
+// alert(3 + 3) // we are using node.js, not browser
+
+// range of number -> pow(2, 53)
+// boolean -> true/false
+// string -> ""
+// null -> standalone value
+// undefined -> value not assigned  
+// symbol -> used to identify unique component (used in REACT and FIGMA)
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
+
---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 140
+let email = "abc@gmail.com"
+var pass = "LG@201201"
+city = "Bangaluru"
+
+// accountId = 4
+// email = "efg@gmail.com"
+pass = "12345"
+city = "Jaipur"
+// console.log(city);  
+console.table([accountId,email, pass, city])
+
+/* NOTE
+    Prefer not to use var, because of the issue in block scope and functional scope
+*/
